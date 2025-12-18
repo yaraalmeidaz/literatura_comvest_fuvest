7f8ca211b461e3bade6f130499b253b41e7e5075
Yara Silva <yaraalmeida869@gmail.com>
2025-12-18 19:08:44 -0300

    configuracao de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: "/projetoextensao",
+  base: "/literatura_comvest_fuvest",
 });
